32c44ed31093ad582554dc64c24e4f541f370f38
MOHAMED ESSA <essa@pencil-designs.com>
2023-03-20 00:04:47 +0200

    delete element

--- src/components/ModalDelete.js
+++ src/components/ModalDelete.js
@@ -8,12 +8,12 @@ const ModalDelete =({modal,toggleDelete,item,handleremove})=> {
             <ModalBody className='border-0 py-4'>
                 <div className='text-center'>
                     <img src={imgwarning} alt="no imag" />
-                    <CardText tag="h5">Are you sure you want to delete {item.name} With Id {item.id}</CardText>
+                    <CardText tag="h5" className='pt-3'>Are you sure you want to delete {item.name} With Price {item.price} $</CardText>
                 </div>
             </ModalBody>
             <ModalFooter className='border-0'>
                 <Button color="light" className='me-auto' onClick={toggleDelete}>Cancel</Button>
-                <Button color="danger" onClick={handleremove}>Delete</Button>
+                <Button color="danger" onClick={handleremove}><i className='fas fa-trash-alt pe-2'></i> Delete</Button>
             </ModalFooter>
         </Modal>
      );
